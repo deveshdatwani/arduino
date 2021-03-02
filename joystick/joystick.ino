c1c2df03a65c5f355d20e76614e5bc2e580a84f1
Deveshd Datwani <datwanidevesh@gmail.com>
2021-03-03 04:06:03 +0530

    joystick servo control

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -1,13 +1,18 @@
+#include<Servo.h>
+Servo servo_test;
+int angle = 0;
 #define JoyX A0
 #define JoyY A1
 float xValue;
 float yValue;
+float angle_val;
 
 void setup() {
   Serial.begin(9600);
   float xValue;
   float yValue;
   pinMode(13,OUTPUT);
+  servo_test.attach(9);
 
 }
 
@@ -25,4 +30,6 @@ void loop(){
   else {
     digitalWrite(13, LOW);
   }
+  angle_val = xValue / 1000;
+  servo_test.write(angle_val*180);
 }
